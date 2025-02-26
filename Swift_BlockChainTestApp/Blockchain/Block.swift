e2c656e9b7cdef6500562193978148c312715484
urabexon <hiroki708.a48uk@gmail.com>
2025-02-26 23:19:21 +0900

    Blockchain dir setting

---
+++ Swift_BlockChainTestApp/Blockchain/Block.swift
@@ -0,0 +1,12 @@
+//
+//  Block.swift
+//  Swift_BlockChainTestApp
+//
+//  Created by Hiroki Urabe on 2025/02/26.
+//
+
+import Foundation
+
+struct Block: Codable {
+    
+}
---
+++ Swift_BlockChainTestApp/Blockchain/Blockchain.swift
@@ -0,0 +1,12 @@
+//
+//  Blockchain.swift
+//  Swift_BlockChainTestApp
+//
+//  Created by Hiroki Urabe on 2025/02/26.
+//
+
+import Foundation
+
+class Blockchain {
+    
+}
