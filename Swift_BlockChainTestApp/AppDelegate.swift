1607a4e9bdb242216397b8edc8f7e1170af57a34
urabexon <hiroki708.a48uk@gmail.com>
2025-02-26 23:31:09 +0900

    setting Delegate Controller Server

---
+++ Swift_BlockChainTestApp/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Swift_BlockChainTestApp
+//
+//  Created by Hiroki Urabe on 2025/02/26.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+}
---
+++ Swift_BlockChainTestApp/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  Swift_BlockChainTestApp
+//
+//  Created by Hiroki Urabe on 2025/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
